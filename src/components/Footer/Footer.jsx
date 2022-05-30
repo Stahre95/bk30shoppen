ca9bf96f1285b24fe7da924cb1a66f2c52028b6f
stahre95 <johan@bk30.se>
2022-05-30 12:42:19 +0200

    added simple footer changed design of home category

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Typography } from '@material-ui/core'
+
+import '../Footer/style.css'
+
+function Footer() {
+  return (
+    <div className='container'>
+       <div className="leftColumn">
+          <Typography variant="subtitle2" color='inherit'>Kontakt</Typography>
+          <Typography variant="subtitle2" color='inherit'>021-41 51 20</Typography>
+          <Typography variant="subtitle2" color='inherit'>info@bk30.se</Typography>
+       </div>
+       <div className="middleColumn">
+          <Typography variant="subtitle2">&copy;Västerås BK30 2022</Typography> 
+       </div>
+       <div className="rightColumn">
+         <Typography variant="subtitle2" color='inherit'>www.bk30.se</Typography>
+       </div>
+    </div>
+  )
+}
+
+export default Footer
