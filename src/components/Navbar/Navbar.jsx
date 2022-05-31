8d0918e875f05b8f32be91501004cf1aa22a6552
Johan <johan@bk30.se>
2022-05-31 10:13:26 +0200

    made navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useState }from 'react'
 import { Link } from 'react-router-dom'
 import { AppBar, Toolbar, IconButton, Badge, Typography } from '@material-ui/core'
 import { ShoppingCart } from '@material-ui/icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faWindowClose } from '@fortawesome/free-solid-svg-icons'
+
 
 import media from '../../assets/js/import'
 import "../Navbar/style.css"
 
 function Navbar({totalItemsInCart}) {
+    const [menuClicked, setMenuClicked] = useState(false);
+
+    const menuActive = () => {
+        if(menuClicked) {
+            return (
+                <div className="navigation-menu-faclose">
+                    <FontAwesomeIcon icon={faWindowClose} size="2x"/>
+                </div>
+            )
+        }else {
+            return (
+                <div className="navigation-menu-fabars">
+                    <FontAwesomeIcon icon={faBars} size="2x"/>
+                </div>
+            )
+        }
+    }
+
+    const handleClick = () => {
+        setMenuClicked(!menuClicked)
+    }
+
     return (
         <>
             <div className="navMain">
@@ -20,28 +45,30 @@ function Navbar({totalItemsInCart}) {
                                 BK Shopen
                             </Link>
                         </Typography>
-                        <div className="grow" />
-                        <div className="right">
-                            <Link to="/klader" className="linkItems" >
+                        <div className="navigation-menu-icon" onClick={handleClick}>
+                            {menuActive()}
+                        </div>
+                        <div className={menuClicked ? "navigation-menu-open" : "navigation-menu"}>
+                            <Link to="/klader" className="linkItems" onClick={handleClick}>
                                 KLÄDER
                             </Link>
-                            <Link to="/souvenir" className="linkItems">
+                            <Link to="/souvenir" className="linkItems" onClick={handleClick}>
                                 SOUVENIR
                             </Link>
-                            <Link to="/ovrigt" className="linkItems">
+                            <Link to="/ovrigt" className="linkItems" onClick={handleClick}>
                                 ÖVRIGT
                             </Link>
                             {/* <Link to="/kontakta-oss" className="linkItems">
                                 KONTAKTA OSS
                             </Link> */}
-                            <IconButton aria-label='Show cart items' color="inherit">
+                        </div>
+                        <IconButton aria-label='Show cart items' color="inherit">
                                 <Badge badgeContent={totalItemsInCart} color="secondary">
                                     <Link to="/varukorg">
                                         <ShoppingCart className='shoppingcart' />
                                     </Link>
                                 </Badge>
                             </IconButton>
-                        </div>
                     </Toolbar>
                 </AppBar>
             </div>
